01a8cfb40f2cf23c6a08d5a966b6449075953115
zhiminC <73890212+923360018@users.noreply.github.com>
2021-08-23 09:07:44 +0800

    Update estimate.R

--- ssGSEA/estimate.R
+++ ssGSEA/estimate.R
@@ -2,7 +2,6 @@ rm(list = ls())
 library(estimate)
 
 inputFile="unisymbol.txt"  
-#运行estimate包
 filterCommonGenes(input.f="unisymbol.txt", 
                   output.f="commonGenes.gct", 
                   id="GeneSymbol")
@@ -10,7 +9,6 @@ filterCommonGenes(input.f="unisymbol.txt",
 estimateScore(input.ds = "commonGenes.gct",
               output.ds="estimateScore.gct")
 
-#输出每个样品的打分
 scores=read.table("estimateScore.gct",skip = 2,header = T)
 rownames(scores)=scores[,1]
 scores=t(scores[,3:ncol(scores)])
